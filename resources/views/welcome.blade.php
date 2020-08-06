657973d859b4044a991948b3aa4862f571340239
Jonathan Salinas <a.jonathan.sl@gmail.com>
2020-08-06 00:28:57 -0500

    laravel ui vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,58 +4,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Series</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    <a href="{{ route('series', ['title' => $title->id]) }}">
-                        {{ $title->originalTitle }}
-                    </a>
-                </div>
-            </div>
+        <div id="app">
+            <example-component></example-component>
         </div>
     </body>
+
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </html>
