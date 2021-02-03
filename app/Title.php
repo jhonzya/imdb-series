912a3534b67a18b9ba5a4f23ef74506f9535bcc7
Jonathan Salinas <a.jonathan.sl@gmail.com>
2021-02-03 00:07:07 -0600

    return types

--- app/Title.php
+++ app/Title.php
@@ -4,6 +4,8 @@ namespace App;
 
 use App\Scopes\SeriesScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Laravel\Scout\Searchable;
 
 class Title extends Model
@@ -19,12 +21,12 @@ class Title extends Model
         static::addGlobalScope(new SeriesScope());
     }
 
-    public function getApiAttribute()
+    public function getApiAttribute(): string
     {
         return route('show', ['title' => $this->attributes['id']]);
     }
 
-    public function getLinkAttribute()
+    public function getLinkAttribute(): string
     {
         $id = str_pad($this->attributes['id'], 7, '0', STR_PAD_LEFT);
         $website = config('imdb.website');
@@ -34,14 +36,14 @@ class Title extends Model
 
     // relationships
 
-    public function episodes()
+    public function episodes(): HasMany
     {
         return $this->hasMany('App\Episode', 'parent_id')
             ->orderBy('seasonNumber')
             ->orderBy('episodeNumber');
     }
 
-    public function rating()
+    public function rating(): HasOne
     {
         return $this->hasOne('App\Rating', 'id');
     }
